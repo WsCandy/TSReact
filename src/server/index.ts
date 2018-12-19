ed0e8b3305462091048a7ef9f172d4543ca77f5d
Samuel Woodbridge <s-bridge@live.co.uk>
2018-12-19 20:34:35 +0000

    [Server] - Allows for an assets argument to be passed to serve static assets via express if necessary

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,10 @@
 import express, { Router } from "express";
+import yargs from "yargs";
 import config from "./config/config";
 import IndexController from "./controllers/IndexController";
+
+const env = process.env.NODE_ENV;
+const args = yargs.argv;
 
 const server = express();
 const router = Router();
@@ -11,5 +15,9 @@ server.set("port", config.port);
 server.set("view engine", config.engine);
 server.set("views", config.views);
 server.listen(config.port);
+
+if (env === "production" && args.assets === "true") {
+    server.use(express.static("dist/public"));
+}
 
 server.get("*", router);
