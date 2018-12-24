cdfbb105aad5f77e8cd522d3770b926082fdb009
Samuel Woodbridge <s-bridge@live.co.uk>
2018-12-24 12:04:51 +0000

    [Client] - Adds promise polyfill for IE

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import "core-js/es6/promise";
 import App from "@containers/App";
 import store, { history } from "@client/store";
 import { ConnectedRouter } from "connected-react-router";
