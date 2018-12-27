5a2430414d186982f6c18601e0ab3499c6095d53
Samuel Woodbridge <s-bridge@live.co.uk>
2018-12-27 14:59:01 +0000

    [React] - Hook test

--- src/common/containers/LoadableContent.tsx
+++ src/common/containers/LoadableContent.tsx
@@ -1,11 +1,22 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {}
 
-const LoadableContent: React.FunctionComponent<Props> = () => (
-    <React.Fragment>
-        <p>Loadable component!</p>
-    </React.Fragment>
-);
+const LoadableContent: React.FunctionComponent<Props> = () => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = count.toString(10);
+    });
+
+    return (
+        <React.Fragment>
+            <p>Loadable component!</p>
+            <button type="button" onClick={() => setCount(count + 1)}>
+                Increase count... {count}
+            </button>
+        </React.Fragment>
+    );
+};
 
 export default LoadableContent;
