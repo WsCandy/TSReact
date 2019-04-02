83909d75008430caa3bde134c185ebf58bebb681
Samuel Woodbridge <s-bridge@live.co.uk>
2019-04-02 21:45:14 +0100

    [Yarn] - Updates dependencies

--- src/server/store/index.ts
+++ src/server/store/index.ts
@@ -1,16 +1,13 @@
 import reducers from "_reducers/index";
 import { applyMiddleware, createStore, Store } from "redux";
 import AppState from "_model/redux/AppState";
-import createMemoryHistory from "history/createMemoryHistory";
+import { createMemoryHistory } from "history";
 import thunk from "redux-thunk";
 import { enableBatching } from "redux-batched-actions";
 
 const store = (url: string): Store<AppState> => {
-    const history = createMemoryHistory({ initialEntries: [url] });
-    return createStore(
-        enableBatching(reducers(history)),
-        applyMiddleware(thunk)
-    );
+    const h = createMemoryHistory({ initialEntries: [url] });
+    return createStore(enableBatching(reducers(h)), applyMiddleware(thunk));
 };
 
 export default store;
